779e637a08cdc800d57f93ec7320e4c3ddbb3dfe
Leandro <leandrojunges21@gmail.com>
2022-07-13 11:50:44 -0300

    feat: creat branch feat/modaldelete modal static

--- src/controller/modal-delete-habit.controller.js
+++ src/controller/modal-delete-habit.controller.js
@@ -1,6 +1,10 @@
 export default class ModalDeleteHabit {
+  static body = document.querySelector("body")
+  
   static render() {
     // renderiza na tela o aviso de que vai deletar o hábito
     // Botão Sim, excluir o hábito deletar hábito na API -> DeleteHabit.delete()
+    const btnErase = document.querySelector("button")
+
   }
 }
