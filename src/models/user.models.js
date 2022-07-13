9d9a8a7c94494b80b409ebc6d72c2ce72e5c9023
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-07-13 16:47:16 -0300

    feat: filter

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,11 +1,33 @@
 import GetAllRequest from "../controller/api-get-all.controller.js";
-
+import Filter from "../controller/filter-homepage.controller.js";
+import { arrHabitFilter } from "../controller/filter-homepage.controller.js";
 export default class User {
   static async finished() {
     // retorna os hábitos da API filtrando somente os hábitos concluídos
   }
 
   static async allPages() {
-    // retorna os hábitos de todas as páginas da API
+
+      let habit = []
+      const allButton = document.querySelector('.main__filterButtonAll')
+
+      allButton.addEventListener('click', async () => {
+        const all = await Filter.allFilter()
+        habit = []
+        habit.push(all)
+        console.log("TESTE")
+      })
+
+      const finishButton = document.querySelector('.main__filterButtonFinish')
+    finishButton.addEventListener('click', async () => {  
+      habit = []
+      const complete = await Filter.habitFilterFinish()
+      habit.push(complete)
+    })
+      // console.log(habit)
+      return habit
   }
 }
+
+
+
--- src/scripts/homepage.scripts.js
+++ src/scripts/homepage.scripts.js
@@ -5,6 +5,8 @@ import ModalEditProfile from "../controller/modal-edit-profile.controller.js";
 import ModalCreateHabit from "../controller/modal-create-habit.controller.js";
 import ModalDeleteHabit from "../controller/modal-delete-habit.controller.js";
 import GetAllRequest from "../controller/api-get-all.controller.js"
+import Filter from "../controller/filter-homepage.controller.js";
+import User from "../models/user.models.js"
 
 ComponentsDom.header1();
 ComponentsDom.userInfo();
@@ -12,6 +14,11 @@ ComponentsDom.main();
 ModalCreateHabit.render();
 ModalUserMenu.render();
 // ModalEditHabit.render();
-ModalDeleteHabit.render();
+// ModalDeleteHabit.render();
+// ModalDeleteHabit.render();
+// User.clickFilterAll();
+
+User.allPages()
+User.finished()
 
 //await ComponentsDom.habitRequest()
