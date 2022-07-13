ba99ff44d2a397b039ef10ff7c56098ead17cf1c
77joaovitor <henrique.joaovitor@gmail.com>
2022-07-13 14:48:29 -0300

    fix: filter

--- src/scripts/homepage.scripts.js
+++ src/scripts/homepage.scripts.js
@@ -5,13 +5,15 @@ import ModalEditProfile from "../controller/modal-edit-profile.controller.js";
 import ModalCreateHabit from "../controller/modal-create-habit.controller.js";
 import ModalDeleteHabit from "../controller/modal-delete-habit.controller.js";
 import GetAllRequest from "../controller/api-get-all.controller.js"
+import Filter from "../controller/filter-homepage.controller.js";
 
 ComponentsDom.header1();
 ComponentsDom.userInfo();
 ComponentsDom.main();
 ModalCreateHabit.render();
 ModalUserMenu.render();
-//ModalEditHabit.render();
+ModalEditHabit.render();
 ModalDeleteHabit.render();
+Filter.allPages();
 
-//await ComponentsDom.habitRequest()
+await ComponentsDom.habitRequest()
